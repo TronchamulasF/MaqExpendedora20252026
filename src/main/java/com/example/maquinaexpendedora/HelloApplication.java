5968cfd3e5d4b77e5088c3ea99757152fdc7f906
marko <mroskos2909@g.educaand.es>
2025-10-07 20:22:15 +0200

    Hice todo el apartado estético (exceptuando imagenes)

--- src/main/java/com/example/maquinaexpendedora/HelloApplication.java
+++ src/main/java/com/example/maquinaexpendedora/HelloApplication.java
@@ -13,6 +13,9 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setMinWidth(1002);
+        stage.setMinHeight(760);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
